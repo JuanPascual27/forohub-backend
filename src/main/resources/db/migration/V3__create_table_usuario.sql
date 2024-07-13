993e521a633502ed6e8ded96e16936fa9a684eaa
Juan Pascual <juanma.pas27@gmail.com>
2024-07-13 14:56:18 -0600

    Add all aplication endpoints

--- src/main/resources/db/migration/V3__create_table_usuario.sql
+++ src/main/resources/db/migration/V3__create_table_usuario.sql
@@ -2,7 +2,7 @@ CREATE TABLE usuario (
     id BIGINT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    contrasena VARCHAR(100) NOT NULL,
+    contrasena VARCHAR(300) NOT NULL,
     perfiles BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (perfiles) REFERENCES perfil(id)
